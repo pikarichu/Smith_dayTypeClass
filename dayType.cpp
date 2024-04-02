b23de588dc97a6cb00969f902f29116507865090
Robert Smith <rrchcuttr@gmail.com>
2024-04-01 22:34:36 -0400

    Added comments to commented out functions and finished the other functions and constructor

--- dayType.cpp
+++ dayType.cpp
@@ -8,6 +8,7 @@ void dayType::setDay(string aDay)
 	int index = 0;
 	for (int i = 0; i < 7; i++)
 	{
+		//cout << dayOfWeek[i]; //Taken from original document
 		if (dayOfWeek[i] == aDay)
 		{
 			index = i;
@@ -20,6 +21,73 @@ void dayType::setDay(string aDay)
 void dayType::print() const
 {
 	cout << day << endl;
+}
+
+string dayType::previousDay()
+{
+	//day = dayOfWeek[0]; //Taken from original document
+	
+	for (int i = 0; i < 7; i++)
+	{
+
+
+		if (dayOfWeek[i] == day)
+		{
+			int newIndex;
+			newIndex = (i - 1) % 7;
+
+			if (newIndex < 0)
+				newIndex = 6;
+			//day = dayOfWeek[newIndex];
+			return dayOfWeek[newIndex];
+			break;
+		}
+		
+	}
+
+}
+
+string dayType::nextDay()
+{
+	//day = dayOfWeek[0]; //Taken from original document
+	
+	for (int i = 0; i < 7; i++)
+	{
+
+		if (dayOfWeek[i] == day)
+		{
+			int newIndex = 0;
+			newIndex = (i + 1) % 7;
+
+			if (newIndex > 6)
+				newIndex = 0;
+
+			//day = dayOfWeek[newIndex]; //Taken from original document
+			return dayOfWeek[newIndex];
+			break;
+		}
+		
+	}
+
+}
+
+void dayType::addDays(int nDays)
+{
+	int index = 0;
+	
+	for (int i = 0; i < 7; i++)
+	{
+		if (dayOfWeek[i] == day)
+		{
+			int newIndex = 0;
+			index = i;
+			newIndex = (index + nDays) % 7;
+			cout << "After adding " << nDays << " days: " << dayOfWeek[newIndex] << endl;
+			break;
+		}
+		
+	}
+	
 }
 
 
